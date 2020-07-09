bac2798121fd7877dc30ed1200a13efec04ec76e
pankokko <teshi41@gmail.com>
2020-07-09 20:45:30 +0900

    update[before merge]

--- app/Service/MeditationService.php
+++ app/Service/MeditationService.php
@@ -16,21 +16,21 @@ class MeditationService
         //今日の瞑想時間だけとってくる
         $tmp = new Carbon('today');
         $today = $tmp->format('Y-m-d');
-        $meditations = Meditation::where('user_id', $userId)->where('date', $today)->get();
+        // $meditations = Meditation::where('user_id', $userId)->where('date', $today)->get();
 
 //        $weekMeditations = Meditation::where('user_id', $userId)->whereBetween('date', [Carbon::now(), Carbon::now()->subDays(7)->toDateTimeString()] )->get();
 
 
         //今日の瞑想時間だけ取得し配列に格納する
-        $meditationTime = [];
-        foreach ($meditations as $meditation) {
-            $meditationTime[] = $meditation->meditation_time;
-        }
+        // $meditationTime = [];
+        // foreach ($meditations as $meditation) {
+        //     $meditationTime[] = $meditation->meditation_time;
+        // }
 
         //今日の瞑想時間の合計値を算出する
-        $meditationSum = array_sum($meditationTime);
+        // $meditationSum = array_sum($meditationTime);
 
-        return [$meditations, $meditationSum];
+        // return [$meditations, $meditationSum];
     }
 
     public function storeMeditationData(array $data)
